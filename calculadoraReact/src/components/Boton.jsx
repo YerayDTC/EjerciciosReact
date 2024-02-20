9023f586f486d3789e1901d48f46d50352f72ed4
yeray <yeray.programacion.web@gmail.com>
2024-02-20 22:36:07 +0000

    diseño calculadora

---
+++ calculadoraReact/src/components/Boton.jsx
@@ -0,0 +1,11 @@
+import './Boton.css'
+
+const Boton = (params) => {
+    const { texto, clase } = params;
+
+  return (
+    <button className={clase}>{texto}</button>
+  )
+}
+
+export default Boton
